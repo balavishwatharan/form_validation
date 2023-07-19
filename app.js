4f9e4cdb04a9f1b5bc7ed77b37ccf10bfd9c8553
balavishwatharan <88933677+balavishwatharan@users.noreply.github.com>
2023-07-19 15:50:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+var createError = require('http-errors')
+var session = require('express-session')
+var flash = require('express-flash')
+var express = require('express')
+var logger = require('morgan')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var db = require('db')
+var app = express()
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use(logger('dev'))
+app.use(express.json())
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(
+  session({
+    secret: '123@123abc',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 },
+  }),
+)
+app.use(flash())
+app.get('/', function (req, res, next) {
+  res.render('index', { title: 'MY FORM' })
+})
+app.post('/user_form', function (req, res, next) {
+  var name = req.body.name
+  var regno = req.body.regno
+  var sql = `INSERT INTO student (name, regno) VALUES ("${name}", "${regno}",)`
+  db.query(sql, function (err, result) {
+    if (err) throw err
+    console.log('Row has been updated')
+    req.flash('success', 'Data stored!')
+    res.redirect('/')
+  })
+})
+app.use(function (req, res, next) {
+  next(createError(404))
+})
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  res.status(err.status || 500)
+  res.render('error')
+})
+app.listen(3036, function () {
+  console.log('Node server is running on port : 3036')
+})
+module.exports = app
